d9ec6f5eab547e8dffb494872bec2a8781ffed08
daniel <danielrodriguescarvalho777@gmail.com>
2023-12-04 17:24:19 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,20 +1,15 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`Id da mensagem: ${id}.
-  Para o usuário: ${user}
-  `);
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Página: ${page}. Mostrar: ${limit}`);
+  response.json({ name, email, password });
 });
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
