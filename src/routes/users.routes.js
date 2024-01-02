1c997466e1d2d333acc947a84f552bfbf25ea531
daniel <danielrodriguescarvalho777@gmail.com>
2024-01-02 18:45:07 -0300

    trying update users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router();
 const usersController = new UsersController();
 
 usersRoutes.post("/users", usersController.create);
+usersRoutes.put("users/:id", usersController.update);
 
 module.exports = usersRoutes;
