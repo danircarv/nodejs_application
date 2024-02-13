16245d79cbb2067d3b8476498e13504f15d67ed3
daniel <danielrodriguescarvalho777@gmail.com>
2024-02-13 17:13:17 -0300

    creating and using user repository

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,7 @@
 const { hash, compare } = require("bcryptjs");
-
 const AppError = require("../utils/AppError");
 
+const UserRepository = require("../repositories/UserRepository");
 const sqliteConnection = require("../database/sqlite");
 const { application } = require("express");
 
@@ -16,11 +16,9 @@ class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExist = await database.get(
-      "SELECT * FROM users WHERE EMAIL = (?)",
-      [email]
-    );
+    const userRepository = new UserRepository();
+
+    const checkUserExist = await userRepository.findByEmail(email);
 
     if (checkUserExist) {
       throw new AppError("Este email já está em uso");
@@ -28,10 +26,7 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8);
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, hashedPassword]
-    );
+    await userRepository.create({ name, email, password: hashedPassword });
 
     return response.status(201).json();
   }
