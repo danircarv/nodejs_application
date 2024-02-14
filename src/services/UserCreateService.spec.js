91cef43508e8b8bc9928fb96d87db4c42c128bf5
daniel <danielrodriguescarvalho777@gmail.com>
2024-02-14 11:27:48 -0300

    test: existing email

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,5 +1,6 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () => {
   it("user should be created", async () => {
@@ -14,5 +15,27 @@ describe("UserCreateService", () => {
     const userCreated = await userCreateService.execute(user);
 
     expect(userCreated).toHaveProperty("id");
+  });
+
+  it("user should'nt be created with existent email", async () => {
+    const user1 = {
+      name: "User Test1",
+      email: "user@test.com",
+      password: "123",
+    };
+
+    const user2 = {
+      name: "User Test2",
+      email: "user@test.com",
+      password: "456",
+    };
+
+    const userRepository = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepository);
+
+    await userCreateService.execute(user1);
+    await expect(userCreateService.execute(user2)).rejects.toEqual(
+      new AppError("Este email já está em uso")
+    );
   });
 });
