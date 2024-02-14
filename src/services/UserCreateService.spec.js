d54dc6190dc810085a47097c460977cd99f69f39
daniel <danielrodriguescarvalho777@gmail.com>
2024-02-14 07:58:48 -0300

    creating user register test

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,1 +1,14 @@
-it("user should be created", () => {});
+const UserCreateService = require("./UserCreateService");
+
+it("user should be created", () => {
+  const user = {
+    name: "User Test",
+    email: "user@test.com",
+    password: "123",
+  };
+
+  const UserCreateService = new UserCreateService();
+  const userCreated = userCreateService.execute(user);
+
+  expect(userCreated).toHaveProperty("id");
+});
